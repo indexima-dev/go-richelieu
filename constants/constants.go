0121a0ac3220950946860ce8b12d4fcdc6727ccf
Sébastien Farges <seb.farges@gmail.com>
2021-07-09 23:31:55 +0200

    Up AppVersion

--- constants/constants.go
+++ constants/constants.go
@@ -3,6 +3,6 @@ package constants
 const (
 	AppName         string = "Richelieu"
 	AppDescription  string = "Data generator that respects cardinality and schema structures#."
-	AppVersion      string = "0.1"
+	AppVersion      string = "0.2"
 	DefaultPlanFile string = "plan.json"
 )
